3fdc5c2151246a387dffab08e553c8df2621c505
Fatima <fatima.b_1998@outlook.fr>
2020-11-06 09:38:29 +0100

    exercice students

--- students.sql
+++ students.sql
@@ -35,3 +35,35 @@ CREATE TABLE favorites (
     student_id VARCHAR(1) NOT NULL,
     PRIMARY KEY (id)
 )
+
+INSERT INTO favorites
+VALUES (1, 'Maths', 'Cricket', 2),
+        (2, 'Music', 'Hip-hop', 6),
+        (3, 'Arts', 'Boxing', 1),
+        (4, 'Literature', '	Tennis', 3),
+        (5, 'Computer science', 'Tennis', 5),
+        (6, 'Arts', 'Baseball', 4);
+
+CREATE TABLE students_languages (
+    id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    student_id VARCHAR(1) NOT NULL,
+    language_id VARCHAR(1) NOT NULL,
+    PRIMARY KEY (id)
+)
+
+INSERT INTO students_languages
+VALUES (1, 1, 1),
+        (2, 1, 2),
+        (3, 2, 1),
+        (4, 2, 3),
+        (5, 3, 1),
+        (6, 4, 1),
+        (7, 4, 2),
+        (8, 4, 4),
+        (9, 4, 5),
+        (10, 5, 1),
+        (11, 5, 5),
+        (12, 6, 1),
+        (13, 6, 2),
+        (14, 6, 3);
+
